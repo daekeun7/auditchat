6b67c165ec5758de3d7b960047ee18ecc8a6a477
Goo Kim <daekeun7@gmail.com>
2024-02-26 16:23:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 # import sys
 # sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.chains import RetrievalQA
 import streamlit as st
 import tempfile
 import os
